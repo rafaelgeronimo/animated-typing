93a68c1431d38c685749867361f988a3d9bf4b79
rafaelgeronimo <rafaelgeronimo@outlook.com>
2019-03-13 13:21:40 -0300

    Animated Typing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Digitação animada com [Typed](https://github.com/mattboldt/typed.js/)
+
+![](assets/img/sample.gif)
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".type", {
+  strings: [
+    "Olá mundo!",
+    "Meu nome é Rafael Gerônimo",
+    "Eu sou um Analista de Suporte formado em Sistemas para Internet"
+  ],
+  typeSpeed: 40,
+  backSpeed: 20,
+  loop: true
+});
